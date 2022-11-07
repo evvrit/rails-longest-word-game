7f26011a8bd9522193b49abd11364649dbd00bdb
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-07 16:21:19 -0500

    the game works

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
 
   def new
@@ -5,5 +8,21 @@ class GamesController < ApplicationController
   end
 
   def score
+    if params[:word]
+      # @message = params[:tiles]
+      @tiles = params[:tiles]
+      (params[:word].split('')).each do |letter|
+        if @tiles.include?(letter)
+          response = URI.open("https://wagon-dictionary.herokuapp.com/#{params[:word]}").read
+          if JSON.parse(response)["found"]
+            @message = "Congratulations! #{params[:word].upcase} is a valid English word!"
+          else
+            @message = "Sorry but #{params[:word].upcase} does not appear to be a valid English word..."
+          end
+        else
+          @message = "Sorry but #{params[:word]} can't be built out of #{@tiles}"
+        end
+      end
+    end
   end
 end
